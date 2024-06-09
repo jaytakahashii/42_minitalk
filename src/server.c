c0418625b2059b76ea139afc62e1f7c6c6527054
Jay Takahashi <qsakura14555@gmail.com>
2024-06-09 16:38:44 +0900

    Quote https://en-junior.com/sigaction-signal/

--- src/server.c
+++ src/server.c
@@ -6,7 +6,44 @@
 /*   By: jtakahas <jtakahas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 15:22:14 by jtakahas          #+#    #+#             */
-/*   Updated: 2024/06/09 15:22:15 by jtakahas         ###   ########.fr       */
+/*   Updated: 2024/06/09 16:29:15 by jtakahas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <signal.h> /* signal.hをインクルード */
+#include <stdlib.h>
+#include <string.h>
+
+int count;
+
+void signal_handler(int signum)
+{
+    /* シグナルハンドラ内で安全ではない関数を使っていない */
+    count += 100;
+}
+
+int main(void){
+
+    struct sigaction sa;
+    /* シグナルマスクのクリア(エラーチェック付き) */
+    if (-1 == sigemptyset(&sa.sa_mask))
+	{
+        exit(1);
+    }
+    sa.sa_handler = signal_handler;
+    sa.sa_flags = 0;
+
+    /* シグナルハンドラの登録(エラーチェック付き) */
+    if(-1 == sigaction(SIGINT, &sa, NULL)){
+        exit(1);
+    }
+
+    /* 変数countが50以下の間ループ */
+    while(count < 50){
+    }
+
+    puts("Count Over.");
+
+    return 0;
+}
